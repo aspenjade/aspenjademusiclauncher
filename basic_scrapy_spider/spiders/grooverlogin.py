f8772ad640ed079506caa487121850ecd3c72822
aspenjade <hsu.aspen@gmail.com>
2024-10-10 09:00:23 -0700

    AspenJade can login - no data

--- basic_scrapy_spider/spiders/grooverlogin.py
+++ basic_scrapy_spider/spiders/grooverlogin.py
@@ -1,6 +1,7 @@
 import scrapy
 import json
 from scrapy.http import FormRequest
+from scrapy.http import HtmlResponse
 
 class LoginSpider(scrapy.Spider):
     name = 'login_spider'
@@ -12,7 +13,7 @@ class LoginSpider(scrapy.Spider):
         # Extract the necessary login credentials here
         login_data = {
             'email': 'aspenjadeartist@gmail.com',
-            'password': 'Rachel*22'
+            'password': 'password'
         }
         
         url = 'https://auth.groover.co/api/login/'
@@ -32,14 +33,62 @@ class LoginSpider(scrapy.Spider):
             self.logger.info("Logged in successfully!")
             # Now you can continue scraping as an authenticated user
             # For example, scraping content from the authenticated pages
-            pass
-            #yield response.follow('https://groover.co/en/band/dashboard/', callback=self.parse_dashboard)
+            yield response.follow('https://groover.co/en/band/dashboard/', callback=self.parse)
 
             
+    def parse(self, response):
+        if 'band/dashboard' in response.url:
+            print('Successfully navigated to the dashboard page.')
+            if self.wait_for_page_load(response):
+            # Process the loaded page data
+                self.logger.info("Page has finished loading: {}".format(response.url))
+            # Call your parse_dashboard method here
+            yield self.parse_dashboard(response)
+        else:
+            print('Navigation to the dashboard page failed. Current URL: %s', response.url)
+
+    def wait_for_page_load(self, response):
+        """
+        Waits for the page to load completely
+        """
+        if isinstance(response, HtmlResponse) and response.body:
+            print('page loaded')
+            html_file = open('random.html', 'w')
+            html_file.write(response.body.decode("utf-8"))
+            # Additional check can be added based on your requirement (e.g. certain element to be present)
+            return True
+        return False
+
 
     def parse_dashboard(self, response):
-        campaigned_songs = response.xpath('/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div/fieldset[1]')
+        if response.url == 'https://groover.co/en/band/dashboard/':
+            print('Successfully navigated to the dashboard page.')
+        print('campaigned_songs')
+        campaigned_songs = response.xpath('//div[@id="ignoreFontUpscale"]/div/fieldset[1]//label/span/text()').extract()
+        print('OKAY')
+        field_set_items = response.css('fieldset')
+        print(field_set_items)
+
+
         print(campaigned_songs)
         # Parse and extract data from the authenticated dashboard page
         # Example: Extracting information from the dashboard page
         pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
